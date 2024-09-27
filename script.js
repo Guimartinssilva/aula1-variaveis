9ea6f76a61762048b8f71dac8b5fc59122b7a2ff
Guimartinssilva <guimartinssilva23@gmail.com>
2024-09-26 21:42:59 -0300

    comandos básicos JavaScript

---
+++ script.js
@@ -0,0 +1,15 @@
+//isso é um erro de Página incial.
+
+/*alert("Página Perigosa")
+console.log("alert");
+*/
+
+//Imprimindo meu nome e minha idade
+const nome = "Guilherme";
+let idade = 23;
+
+console.log("Meu nome é " + nome + "e tenho " + idade + "anos.");
+
+let estado = prompt("Onde você mora?");
+
+alert("Você mora em " + estado);
